72331999ad3783a2eaab0232dd17c4cfe1edc2e6
Phyo Hein ( Ezekiel ) <154808657+phyo-105438@users.noreply.github.com>
2024-07-28 18:30:53 +0700

    Initial Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,6 +25,10 @@ android {
             )
         }
     }
+
+    buildFeatures{
+        viewBinding=true
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -37,6 +41,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation("com.github.chanmratekoko:myanmar-calendar:1.0.9.RELEASE")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/example/myanmarcalendar/MainActivity.java
+++ app/src/main/java/com/example/myanmarcalendar/MainActivity.java
@@ -1,24 +1,58 @@
 package com.example.myanmarcalendar;
-
+import android.app.DatePickerDialog;
 import android.os.Bundle;
-
+import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import com.example.myanmarcalendar.databinding.ActivityMainBinding;
+import java.util.Locale;
+import mmcalendar.MyanmarDate;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private DatePickerDialog datePickerDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        initDialog();
+        initListeners();
+    }
+
+    private void initDialog(){
+        datePickerDialog = new DatePickerDialog(this);
+        datePickerDialog.setOnDateSetListener((view, year, month, dayOfMonth) -> {
+            binding.etDate.setText(String.format(Locale.ENGLISH, "%d/%d/%d",dayOfMonth,month +1 , year));
+        });
+    }
+
+    private void initListeners(){
+        binding.btnCalculate.setOnClickListener(v -> {
+            try {
+                //Format 01/01/1970
+                String[] date = binding.etDate.getText().toString().split("/");
+                if(date.length !=3) throw new Exception("Invalid date");
+                //Setting Array Index
+                int day = Integer.parseInt(date[0]);
+                int month = Integer.parseInt(date[1]);
+                int year = Integer.parseInt(date[2]);
+                MyanmarDate mmdate = MyanmarDate.of(year, month , day);
+//                String mmYear = mmdate.getYear();
+//                String mmMonth = mmdate.getMonthName();
+//                String mmDay = mmdate.getMoonPhase() + " " + mmdate.getFortnightDay();
+//                binding.txt.setText(String.format(Locale.getDefault() ,"%s %s %s", mmYear , mmMonth , mmDay));
+                  binding.txt.setText(mmdate.format("B y k, M p f r E n"));
+            }
+            catch(Exception e){
+                Toast.makeText(MainActivity.this, "Invalid Date", Toast.LENGTH_SHORT).show();
+            }
+        });
+        binding.btnSelect.setOnClickListener(v -> {
+            datePickerDialog.show();
         });
     }
 }
